28f6f91a6cd0970571ef932e1d0f2a5aa1b817bd
farwa-faraz <99012312+farwa-faraz@users.noreply.github.com>
2023-07-04 18:41:46 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,28 @@ function setup()
 {
     canvas = createCanvas(500, 500)
     canvas.center()
+
+    video = createCapture(VIDEO)
+    video.size(300, 300)
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('It is loaded');
+}
+
+function draw()
+{
+    background('white');
 }
