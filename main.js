76759975d951f51fb17b9b8c47f6af41af21c7d9
farwa-faraz <99012312+farwa-faraz@users.noreply.github.com>
2023-07-13 17:16:32 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 function setup()
 {
-    canvas = createCanvas(500, 500)
+    canvas = createCanvas(400, 400)
     canvas.center()
 
     video = createCapture(VIDEO)
-    video.size(300, 300)
+    video.size(200, 200)
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+
+leftWristX = 0
+rightWristX = 0
+difference = 0
 
 function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log('difference:'+ difference);
     }
 }
 
@@ -26,4 +35,7 @@ function modelLoaded()
 function draw()
 {
     background('white');
+    textSize(difference);
+    fill('blue');
+    text('Hi', 10, 100);
 }
